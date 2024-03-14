676e8fa9d5f34f77c0e85b867a4bfb6c9c7338fc
heung88 <beehive2838@gmail.com>
2024-03-14 19:03:53 +0900

    feat : add route

---
+++ app/(route)/(public)/auth/findpw/page.tsx
@@ -0,0 +1,10 @@
+import { FindPw } from "@/app/components/auth/findpw/FindPw";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <FindPw />
+    </>
+  );
+}
---
+++ app/(route)/(public)/auth/signin/page.tsx
@@ -0,0 +1,10 @@
+import { Login } from "@/app/components/auth/signin/Login";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
