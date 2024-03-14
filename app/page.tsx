3e5b67d57d3e92d27fca9d0d96d97dbbf906a226
heung88 <beehive2838@gmail.com>
2024-03-14 16:17:18 +0900

    fix : fix address

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { Login } from "./components/login/Login";
+import { Login } from "./components/auth/signin/Login";
+import { Signup } from "./components/auth/signup/Signup";
 
 export default function Home() {
   return (
     <>
       <Login />
+      {/* <Signup /> */}
     </>
   );
 }
