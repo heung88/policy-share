2d45390326e1827115b99f90fbe7c9e8855750be
heung88 <beehive2838@gmail.com>
2024-03-15 14:16:51 +0900

    feat : feature main nav

---
+++ app/components/menu/Nav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <nav className="main-category">
+      <ul>
+        <li>메뉴1</li>
+        <li>메뉴2</li>
+        <li>메뉴3</li>
+        <li>메뉴4</li>
+      </ul>
+    </nav>
+  );
+};
