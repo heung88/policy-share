211211e442a298e9d4359459d7edefac23b9cb47
heung88 <beehive2838@gmail.com>
2024-03-15 17:54:48 +0900

    fix : move subcategory

--- app/components/main/admin/menu/AdminNav.tsx
+++ app/components/main/admin/menu/AdminNav.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { ManagementSubCategory } from "@/app/components/management/ManagementSubCategory";
 import React from "react";
 
 export const AdminNav = () => {
@@ -9,10 +9,14 @@ export const AdminNav = () => {
           <strong>관리자 메뉴</strong>
         </li>
         <li>
-          <Link href={"/admin/management"}>대리점/판매점 관리</Link>
+          {/* <Link href={"/admin/management"}>대리점/판매점 관리</Link> */}
+          <input type="radio" name="management" id="adminManagement" defaultChecked />
+          <label htmlFor="adminManagement">대리점/판매점 관리</label>
+          <ManagementSubCategory />
         </li>
         <li>
-          <Link href={"/"}>정책 관리</Link>
+          <input type="radio" name="management" id="adminPolicy" />
+          <label htmlFor="adminPolicy">정책 관리</label>
         </li>
       </ul>
     </nav>
